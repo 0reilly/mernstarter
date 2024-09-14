ff4b60abcec7b43f53b321566a1dccd6625ef1d3
0reilly <oreilly.119@osu.edu>
2024-09-14 15:42:19 -0400

    fixed icon libarary

--- frontend/src/components/AppIcon.js
+++
@@ -1,9 +0,0 @@
-import { Email } from '@mui/icons-material';
-
-const AppIcon = () => (
-  <div className="w-12 h-12 bg-black rounded-lg flex items-center justify-center">
-    <Email className="text-white" style={{ fontSize: 32 }} />
-  </div>
-);
-
-export default AppIcon;
