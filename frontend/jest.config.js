c0fe1922ccfd34c29f5e6d549afce2eca1002a43
0reilly <oreilly.119@osu.edu>
2024-12-05 21:07:35 -0500

    update jest

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,9 +2,13 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!axios)',
+    '/node_modules/(?!axios)'
   ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  }
 };
--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,3 +1,18 @@
 import '@testing-library/jest-dom';
 import '@testing-library/react';
 import '@testing-library/user-event';
+
+// Mock window.matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
